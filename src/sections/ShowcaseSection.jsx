9b54acf2c02f7993a8c715d4a48fe45c38996735
Slothdemon <www.basilslothdemon@gmail.com>
2025-07-02 02:18:42 +0500

    changes

--- src/sections/ShowcaseSection.jsx
+++ src/sections/ShowcaseSection.jsx
@@ -49,12 +49,12 @@ const AppShowcase = () => {
         <div className="showcaselayout">
           <div ref={rydeRef} className="first-project-wrapper">
             <div className="image-wrapper">
-              <img src="/images/project1.png" alt="Ryde App Interface" />
+              <img src="/images/cars.png" alt="Ryde App Interface" />
             </div>
             <div className="text-content">
               <h2>
                 On-Demand Rides Made Simple with a Powerful, User-Friendly App
-                called Ryde
+                called CarRental
               </h2>
               <p className="text-white-50 md:text-xl">
                 An app built with React Native, Expo, & TailwindCSS for a fast,
@@ -65,20 +65,20 @@ const AppShowcase = () => {
 
           <div className="project-list-wrapper overflow-hidden">
             <div className="project" ref={libraryRef}>
-              <div className="image-wrapper bg-[#FFEFDB]">
+              <div className=" ">
                 <img
-                  src="/images/project2.png"
+                  src="/images/depin.png"
                   alt="Library Management Platform"
                 />
               </div>
-              <h2>The Library Management Platform</h2>
+              <h2>Decentralized Uptime Monitoring System</h2>
             </div>
 
             <div className="project" ref={ycDirectoryRef}>
-              <div className="image-wrapper bg-[#FFE7EB]">
-                <img src="/images/project3.png" alt="YC Directory App" />
+              <div className=" ">
+                <img src="/images/course.png" alt="YC Directory App" />
               </div>
-              <h2>YC Directory - A Startup Showcase App</h2>
+              <h2>An Ai Powered Education Platform</h2>
             </div>
           </div>
         </div>
